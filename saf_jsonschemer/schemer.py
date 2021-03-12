807c661136769a4dfb734760a0567ea5e06a9f36
18826377 <IABarashev@sberbank.ru>
2021-03-12 22:00:08 +0300

    The code

---
+++ saf_jsonschemer/schemer.py
@@ -0,0 +1,114 @@
+import os
+import json
+from pathlib import Path
+import jsonschema
+
+
+class JsonSchemer:
+    """
+    Allows to use JSON-Schema repo with many linked schema files.
+    """
+
+    def __init__(self, schema_root, schema_folders):
+        self.schema_uris, self.schema_values = dict(), dict()
+        self._load_schemas_repo(schema_root, schema_folders)
+
+    def _index_schemas_folder(self, schema_folder, schema_ext='.json'):
+        """
+        Add local schema instances from some folder.
+
+        :param schema_folder: path to schema repo
+        :type schema_folder: Path
+        :param schema_ext: extension of JSON-schema files
+        :type schema_ext: str
+
+        Arguments:
+            schema_uris (Dict[str, str]): a map to save schema path by its name
+            schema_values (Dict[str, str]): a map to save content path by its name
+            schema_folder (str): the local folder of the schemas.
+            schema_ext (str): filter files with this extension in the schema_folder
+        """
+        # print(f'{schema_folder}:')
+        for dir, _, files in os.walk(schema_folder):
+            for file in files:
+                if file.endswith(schema_ext):
+                    schema_path = Path(dir) / Path(file)
+                    with open(schema_path) as schema_file:
+                        schema_doc = json.load(schema_file)
+                    key = f'/{schema_path}'
+                    name = schema_path.name.replace(schema_ext, '')
+                    if name in self.schema_uris:
+                        raise ValueError(
+                            f'Double name {name}'
+                            f' old from {self.schema_uris[name]}'
+                            f' new from {key}'
+                        )
+                    self.schema_uris[name] = key
+                    self.schema_values[name] = schema_doc
+                    # print(f'Added {name} : {key}')
+
+    def _load_schemas_repo(self, schema_root, schema_folders):
+        """
+        Add local schema instances to a storage.
+
+        :param schema_root: path to schema repo
+        :type schema_root: Path
+        :param schema_root: an iterable of repo folder names
+        :type schema_root: Iterable[str]
+        """
+        schema_uris = dict()
+        schema_values = dict()
+        schema_root = Path(os.path.abspath(schema_root))
+        schema_paths = [schema_root / folder for folder in schema_folders]
+
+        for path in schema_paths:
+            self._index_schemas_folder(path)
+
+        return schema_uris, schema_values
+
+    def check_type(self, name):
+        """
+        For debug purposes.
+        :param name: schema name to check
+        :type name: str
+        """
+        print(f'Checking schema: "{name}"')
+        print('\tURI:', self.schema_uris[name])
+        print('\tContent:', self.schema_values[name])
+
+    def validate(self, instance, instance_type=None):
+        """
+        Validate an object using given or guessed schema.
+
+        :param instance: object to validate
+        :type instance: dict
+        :param instance_type: object type name
+        :type instance_type: Optional[str]
+        """
+        assert isinstance(instance, dict)
+        if instance_type is None:
+            if 'type' in instance:
+                # Usual nice case
+                instance_type = instance['type']
+                # print(f'Instance type explicit: "{instance_type}"')
+            else:
+                # Take single nested object
+                # and use its key as a type name
+                assert len(instance) == 1
+                instance_type = list(instance.keys())[0]
+                instance = instance[instance_type]
+                # print(f'Instance type by key: "{instance_type}"')
+
+        schema = self.schema_values[instance_type]
+        base_uri = f'{Path(self.schema_uris[instance_type]).parent}/'
+
+        # print('Base uri:', base_uri)
+        # print(f'Schema:', schema)
+
+        # Resolver can be created only with referrer schema
+        # So we need a unique resolver for each schema or data instance
+        resolver = jsonschema.RefResolver(base_uri=base_uri, referrer=schema)
+        for name in self.schema_values:
+            resolver.store[self.schema_uris[name]] = self.schema_values[name]
+
+        jsonschema.validate(instance, schema, resolver=resolver)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="saf_patterns",
+    author="SberDevices",
+    author_email="developer@sberdevices.ru",
+    description="SAF JSON Schemer — это плагин для SmartApp Framework, "
+                "который позволяет валидировать сообщения.",
+    long_description_content_type="text/markdown",
+    license="sberpl-2",
+    packages=find_packages(exclude=[]),
+    package_data={
+        "saf_patterns": ["schemas/*.json"]
+    },
+    include_package_data=True,
+    install_requires=[
+        'smart_app_framework',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
