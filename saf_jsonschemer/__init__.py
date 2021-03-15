57db647c323f34b3898b4c7de4e86560e799d0bf
18826377 <IABarashev@sberbank.ru>
2021-03-16 01:00:30 +0300

    Added print_jsonschema_error

--- saf_jsonschemer/__init__.py
+++ saf_jsonschemer/__init__.py
@@ -1,7 +1,11 @@
 from typing import Optional
 from pathlib import Path
 
+import jsonschema
+
 from core.message.msg_validator import MessageValidator
+import core.logging.logger_constants as log_const
+from core.logging.logger_utils import log
 
 from saf_jsonschemer.schemer import JsonSchemer
 
@@ -14,6 +18,31 @@ schema_folders = (
 default_schemer = JsonSchemer(schema_root, schema_folders)
 
 
+def print_jsonschema_error(ex: jsonschema.ValidationError):
+    """
+    In consistence with SmartAppFromMessage.print_validation_error
+    """
+    params = {
+        log_const.KEY_NAME: log_const.EXCEPTION_VALUE
+    }
+    if ex.validator == "type":
+        params["required_field_type"] = ex.validator_value
+        params["required_field"] = ex.path[0]
+        msg = (
+            "Payload validation error: Expected '%(required_field)s'"
+            " of type '%(required_field_type)s'"
+        )
+    elif ex.validator == "required":
+        params["required_field"] = ex.validator_value
+        msg = (
+            "Payload validation error: Required field "
+            "'%(required_field)s' is missing"
+        )
+    else:
+        msg = f"Message validation error: Format is wrong: {ex.message}"
+    log(msg, params=params, level="ERROR")
+
+
 class ByNameMessageValidator(MessageValidator):
     def _get_schema_by_message(self, message_name: str) -> Optional[str]:
         if message_name in default_schemer.schema_values:
@@ -23,9 +52,18 @@ class ByNameMessageValidator(MessageValidator):
     def validate(self, message_name: str, data: dict):
         schema_name = self._get_schema_by_message(message_name)
         if schema_name is not None:
-            default_schemer.validate(data, schema_name)
+            try:
+                default_schemer.validate(data, schema_name)
+            except jsonschema.ValidationError as ex:
+                print_jsonschema_error(ex)
+                return False
 
 
 class ToClientMessageValidator(MessageValidator):
     def validate(self, message_name: str, data: dict):
-        default_schemer.validate(data, 'from_nlp_to_client')
+        try:
+            default_schemer.validate(data, 'from_nlp_to_client')
+        except jsonschema.ValidationError as ex:
+            print_jsonschema_error(ex)
+            return False
+
